bbc5993921eb45cacce0f958bfb412d70366ebbd
christopherivy <cdi.747@gmail.com>
2021-05-17 14:26:38 -0700

    git-post

---
+++ app.js
@@ -0,0 +1,37 @@
+/* 
+In your own terms, define the following terms:
+
+What is HTTP?
+   -hyepertext transfer protocol
+
+What is a URL?
+   -a link that takes you to a web page
+
+What is DNS?
+   -domain naming system. converts site names to ip addresses
+
+What is a query string?
+   -a string built based on key/value entries by the user 
+
+What are two HTTP verbs and how are they different?
+   -GET and POST
+   GET - pulls up the existing data from the server
+   POST - allows you to add and post to the server as new data
+
+
+What is an HTTP request?
+   -a request is when you point your browser to a specific webpage.
+
+
+What is an HTTP response?
+   - HTML from the page your requested including the headers for the page, and other content, caching, cookies etc.
+
+
+What is an HTTP header? Give a couple examples of request and response headers you have seen.
+   -The header shows you the content that makes up the html page, including the language and other files that are required to complete the request to build the page.
+
+
+What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+   -a request is made from the browser to the server and the server responds with the data related to what was requested. 
+
+   */
